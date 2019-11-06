bf8a3c5c1f2719017a8c458d4fdeeb3a284f6073
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2019-11-06 21:15:31 +0200

    text adaugat

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("testing a new version 123");
+		System.out.println("Text nou");
+
 	}
 }
